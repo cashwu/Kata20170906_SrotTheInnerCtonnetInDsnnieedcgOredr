a502dfb4b4f7cdc97df8f41b371a4c1827e6f805
cash <cash@cashwu.com>
2017-09-06 09:02:29 +0800

    Add project files.

---
+++ Kata20170906_SrotTheInnerCtonnetInDsnnieedcgOredr/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Kata20170906_SrotTheInnerCtonnetInDsnnieedcgOredr")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kata20170906_SrotTheInnerCtonnetInDsnnieedcgOredr")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f340e31d-fd0e-4a65-9537-90391e7de236")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
